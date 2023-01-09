af514e7eecce19f39723ad0763c769104bcfea96
gleidson <angelolustosa@gmail.com>
2023-01-09 09:53:09 -0300

    aula 1 de logica de programação

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// document.getElementById("referencia").innerHTML = "<h1>Hello world</h1>"; // Injeta codigo no index.html
+// console.log("No console"); // Imprime no console do inspecionar elemento
+//alert("Ola mundo!"); // Faz aparecer um popup na tela
+
